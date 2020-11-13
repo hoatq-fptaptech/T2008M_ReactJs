18aef11ae0b6cf6931a97a90aadd4fcd028d8f04
hoatq-fptaptech <65893435+hoatq-fptaptech@users.noreply.github.com>
2020-11-13 09:29:20 +0700

    up code fetch api

--- src/App.js
+++ src/App.js
@@ -6,102 +6,50 @@ import Main from "./Main";
 import Detail from "./Detail";
 import Category from "./Category";
 import Login from "./Login";
-const products=[
-    {
-        id:1,
-        name:"Flying Ninja",
-        image:"imgs/product.jpg",
-        price: 12,
-        star:5
-    },
-    {
-        id:2,
-        name:"Hiphop paint",
-        image:"imgs/product.jpg",
-        price: 12,
-        star:1
-    },
-    {
-        id:3,
-        name:"T Shirt 122",
-        image:"imgs/product.jpg",
-        price: 12,
-        star:2
-    },
-    {
-        id:4,
-        name:"Flying Ninja",
-        image:"imgs/product.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        id:5,
-        name:"Flying Ninja",
-        image:"imgs/product.jpg",
-        price: 12,
-        star:4.5
-    },
-    {
-        id:6,
-        name:"Flying Ninja",
-        image:"imgs/product.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        id:7,
-        name:"Flying Ninja",
-        image:"imgs/product.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        id:8,
-        name:"Flying Ninja",
-        image:"imgs/product.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        id:9,
-        name:"Flying Ninja",
-        image:"imgs/product.jpg",
-        price: 12,
-        star:3.5
-    },
-    {
-        id:10,
-        name:"Flying Ninja",
-        image:"imgs/product.jpg",
-        price: 12,
-        star:3.5
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            products:[]
+        }
+    }
+    componentDidMount() {
+        // lay du lieu tu api roi cho vao products
+        fetch("https://foodgroup.herokuapp.com/api/today-special")
+            .then(rs=>rs.json())
+            .then(rs=> {
+                this.setState({
+                    products:rs.data
+                })
+            })
+    }
+    render() {
+        const products = this.state.products;
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Header/>
+                    <Nav/>
+                    <Switch>
+                        <Route exact path="/">
+                            <Main productlist={products}/>
+                        </Route>
+                        <Route exact path="/detail/:id">
+                            <Detail productlist={products}/>
+                        </Route>
+                        <Route exact path="/category">
+                            <Category/>
+                        </Route>
+                        <Route exact path="/login">
+                            <Login/>
+                        </Route>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
     }
 
-];
-function App() {
-  return (
-  <BrowserRouter>
-    <div className="App">
-            <Header/>
-            <Nav/>
-            <Switch>
-                <Route exact path="/">
-                    <Main productlist={products}/>
-                </Route>
-                <Route exact path="/detail/:id">
-                    <Detail productlist={products}/>
-                </Route>
-                <Route exact path="/category">
-                    <Category/>
-                </Route>
-                <Route exact path="/login">
-                    <Login/>
-                </Route>
-            </Switch>
-        </div>
-  </BrowserRouter>
-  );
 }
 
 export default App;
